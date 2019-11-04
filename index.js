b4b629aa54402381cdb92894b132677b6b03baa3
Nikita Dmitriev <nik.dmtrv17@gmail.com>
2019-11-04 14:20:27 +0300

    Release 1

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const fs = require('fs');
 let file = 'choto.txt';
 let fileData = '';
-let promise = new Promise((resolve, reject) => {
+// Release 0 
+let readPromise = new Promise((resolve, reject) => {
     fs.readFile(file, 'utf8', (err, data) => {
         if (err) reject(err);
         fileData = data;
         resolve();
     });
 }).then(() => { console.log(fileData) });
+// Release 1 
+let writePromise = new Promise((resolve, reject) => {
+    fs.writeFile('blabla.txt', 'blablablabla', 'utf8', (err) => {
+        if (err) reject(err);
+        resolve()
+    })
 
-promise
+})
+
+let appendPromise = new Promise((resolve, reject) => {
+    fs.appendFile('blabla.txt', '\nprivet', 'utf8', (err) => {
+        if (err) reject(err);
+        resolve();
+    })
+})
+
+// writePromise;
+// appendPromise;
