c9f00c089c995be19758ac721ea46469b2feab5a
Alex <petenkov2822@gmail.com>
2022-06-16 18:09:44 +0300

    Release 0-1 done

--- format.js
+++ format.js
@@ -1,36 +1,20 @@
-const fs = require('fs');
-const { readDir, stat, rename} = require('./index');
-const wayDir = './notes/'
-
+const {
+  rename,
+  get_stat,
+  read_dir
+} = require('./index.js')
 
 function rundomString() {
-  return Math.random().toString(36).substring(8);
-}
-
-readDir(wayDir)
-  .then(arr => {
-    for (let i = 0; i < arr.length; i++) {
-      stat(`${wayDir}${arr[i]}`)
-        .then(stat => {
-          rename(`${wayDir}${arr[i]}`, `${wayDir}file-${rundomString()}-${stat.size}-${stat.birthtime}`)
-    })
-  }
-})
-.catch(err => console.log(err))
+  return Math.random().toString(36).substring(8)
+};
 
 
-
-///  Примеры вызова функций из экспорта для отработки с возвратом данных а не промисов
-
-
-// readDir(wayDir)
-// .then(result => console.log(result))
-
-// // или :
-
-// async function random() {
-//   let result = await readDir(wayDir)
-//   console.log(result);
-// }
-
-// random()
+read_dir('./notes')
+  .then(arrNamesFiles => {
+    for (let i = 0; i < arrNamesFiles.length; i++) {
+      get_stat(`./notes/${arrNamesFiles[i]}`)
+        .then(stats => {
+          rename(`./notes/${arrNamesFiles[i]}`, `./notes/note-${rundomString()}-${stats.size}-${stats.birthtime}`)
+        });
+    };
+  });
--- index.js
+++ index.js
@@ -1,78 +1,77 @@
 const fs = require('fs');
-const wayDir = './notes/'
 
 
-function readFile(path) {
+function read_file(path, codeRead) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (!err) {
-        resolve(data)
-      } else {
+    fs.readFile(path, codeRead, (err, content) => {
+      if (err) {
         reject(err)
-        console.log('***ОШИБКА***')
-      }
-    })
+      } else {
+        resolve(content)
+      };
+    });
   });
-}
-
-// readFile('./notes/note-1.txt')
-//   .then(result => console.log(result))
-//   .catch(err => console.log(err))
-
-
-function readDir(path) {
-  return new Promise((resolve, reject) => {
-    fs.readdir(path, (err, arrFiles) => {
-      if (!err) {
-        resolve(arrFiles)
-      } else {
-        reject(err)
-        console.log('***ОШИБКА***');
-      }
-    })
-  })
-}
-
-// readDir(wayDir)
-//   .then(result => console.log(result))
-//   .catch(error => console.log(error));
-
-
-function stat(path) {
-  return new Promise((resolve, reject) => {
-    fs.stat(path, (err, stats) => {
-      if (!err) {
-        resolve(stats)
-      } else {
-        reject(err)
-        console.log('***ОШИБКА***')
-      }
-    })
-  })
 };
 
-// stat('./notes/note-1.txt')
-//   .then(result => console.log(result))
-//   .catch(err => console.log(err));
+// read_file('./notes/note-1.txt', 'utf-8')
+//   .then(data => console.log(data));
 
-
-
-function rename(oldWay, newway) {
+function read_dir(path) {
   return new Promise((resolve, reject) => {
-    fs.rename(oldWay, newway, (err) => {
-      resolve()
-    })
-  })
-}
+    fs.readdir(path, (err, arrFilesName) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(arrFilesName)
+      };
+    });
+  });
+};
+
+// read_dir('./notes')
+//   .then(data => console.log(data));
+
+function get_stat(pathAndName) {
+  return new Promise((resolve, reject) => {
+    fs.stat(pathAndName, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      };
+    });
+  });
+};
+
+// get_stat('./notes/note-2.txt')
+//   .then(data => console.log(data));
 
 
-// rename('./renameNotes/NOTE-1.txt', './notes/note-1.txt')
+///// Вариант на async - await :
 
 
-module.exports = {
-  stat,
-  readDir,
-  rename
-}
+// async function x() {
+//   const y = await get_stat('./notes/note-1.txt');
+//   console.log(y);
+// };
+// x();
 
 
+function rename(oldPathAndName, newPathAndName) {
+  return new Promise((resolve, reject) => {
+    fs.rename(oldPathAndName, newPathAndName, (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve('File renamed')
+      };
+    });
+  });
+};
+
+// rename('./notes/note-1.txt', './notes/XXxxxxxxX.txt')
+//   .then(data => console.log(data));
+
+
+
+module.exports = {rename, get_stat, read_dir}
